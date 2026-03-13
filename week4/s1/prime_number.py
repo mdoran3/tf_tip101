34e971cc12ad65939aff2cc86cbf69c2fde46f53
Mitchell <mitchelld.dev@gmail.com>
2026-03-13 05:44:31 -0500

    Create UPI files for week4 session1 version1

---
+++ week4/s1/prime_number.py
@@ -0,0 +1,11 @@
+### U - Understand 
+# 1. Share 2 questions you would ask to help understand the question:
+    #
+
+### P - Plan
+# 2. Write out in plain English what you want to do: 
+
+# 3. Translate each sub-problem into pseudocode:
+
+### I - Implement
+# 4. Translate the pseudocode into Python and share your final answer:
