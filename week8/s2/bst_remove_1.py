b0f9f1433903fd60da607280420cab9959f59367
Mitchell <mitchelld.dev@gmail.com>
2026-04-10 01:07:34 -0500

    Implement remove_bst

--- week8/s2/bst_remove_1.py
+++ week8/s2/bst_remove_1.py
@@ -18,11 +18,46 @@
 
 ### U - Understand 
 # 1. Share 2 questions you would ask to help understand the question:
+    # How do we search in a Binary Search Tree?
+    # Once we find the right node to remove, will it matter if that node 
+    #   (1) is a leaf node, (2) has one child, (3) or has two children?
 
 ### P - Plan
 # 2. Write out in plain English what you want to do: 
+    # if the key is less than the key of the root, recursively call remove_bst with root.left
+    # if the key is greater than the key of the root, recursively call remove_bst with root.right
+    # if neither of these cases are true, we have found the target node
+    # if this located node has no children, return None (essentially deleting that node)
+    # if the located node has one child
+        # if node has left right -> return root.right
+        # if node has right child -> return root.left
+    # if located node has 2 children
+        # successor = root.right (start at the right child of the located node)
+        # while loop though while root.left is not None and set successor = root.left every time
+        # once root.left is None, we have located the next node in the chronological tree structure
+        # set root.val and root.val = to successor.val and successor.key
+        # now remove the successor from the right subtree by calling remove_bst(root.right, successor.key)
 
 # 3. Translate each sub-problem into pseudocode:
+    # if root = None -> return None
+    # if key < root.key
+        # root.left = removal_func(root.left, key)
+    # if key > root.key
+        # root.right = removal_func(root.right, key)
+    # else -> we have found the node
+        # if root.left and root.rigth == None:
+            # return None (target node has no children)
+        # if root.left is None (target node has one child)
+            # return root.right
+        # if root.right is None
+            # return root.leftb (target node has one child)
+        # successor = root.right
+        # while successor is not None (iterate to the left to find the next ordered value)
+            # successor = successor.left
+        # root.key = successor.key (update root to successor value)
+        # root.val = successor.val
+
+        # root.right = removal_func(root.right, successor.key) : (this prunes the successor from the right subtree)
 
 ### I - Implement
 # 4. Translate the pseudocode into Python and share your final answer:
@@ -69,7 +104,23 @@ def remove_bst(root, key):
 
     return root
 
-
+###############
+## PRINT BST ##
+###############
+def print_bst(root):
+    if root is None:
+        print("(empty tree)")
+        return
+    queue = [root]
+    while queue:
+        node = queue.pop(0)
+        left_key  = node.left.key  if node.left  else None
+        right_key = node.right.key if node.right else None
+        print(f"key={node.key}, val={node.val}, left={left_key}, right={right_key}")
+        if node.left:
+            queue.append(node.left)
+        if node.right:
+            queue.append(node.right)
 
 # Example Input Tree #1: (tree depicted using keys) 
 
@@ -79,8 +130,9 @@ def remove_bst(root, key):
 #    5      15
 #   / \     / \
 #  1   8   13  16
-
-
+bt1 = TreeNode(10, None,
+        TreeNode(5, None, TreeNode(1, None), TreeNode(8, None)),
+        TreeNode(15, None, TreeNode(13, None), TreeNode(16, None)))
 # Input: root = 10, key = 10
 # Expected Output: 13
 # Expected Output Tree:
@@ -102,8 +154,10 @@ def remove_bst(root, key):
 #   / \     / \
 #  1   8   13  16
 #       \
-#        9 
-
+#        9
+bt2 = TreeNode(10, None,
+        TreeNode(5, None, TreeNode(1, None), TreeNode(8, None, None, TreeNode(9, None))),
+        TreeNode(15, None, TreeNode(13, None), TreeNode(16, None)))
 # Input: root = 10, key = 8
 # Expected Output: 10 (Should return a node object)
 # Expected Output Tree
@@ -125,8 +179,10 @@ def remove_bst(root, key):
 #   / \     / \
 #  1   8   13  16
 #       \
-#        9 
-
+#        9
+bt3 = TreeNode(10, None,
+        TreeNode(5, None, TreeNode(1, None), TreeNode(8, None, None, TreeNode(9, None))),
+        TreeNode(15, None, TreeNode(13, None), TreeNode(16, None)))
 # Input: root = 10, key = 9
 # Expected Output: 10 (Should return a node object)
 # Expected Output Tree
@@ -137,3 +193,26 @@ def remove_bst(root, key):
 #    5      15
 #   / \     / \
 #  1   8  13  16
+
+
+#################
+##### TEST ######
+#################
+print("################")
+print("FIRST BST")
+print("################")
+print_bst(remove_bst(bt1, 10))
+print()
+print()
+print("################")
+print("SECOND BST")
+print("################")
+print_bst(remove_bst(bt2, 8))
+print()
+print()
+print("################")
+print("THIRD BST")
+print("################")
+print_bst(remove_bst(bt3, 9))
+print()
+print()
